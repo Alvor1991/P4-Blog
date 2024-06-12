1a336ea86eb627e148dee609e9ee075cea456b68
Ryan Nolan <ryannolan_@hotmail.com>
2024-06-12 13:16:34 +0000

    Connect to database

--- codestar/settings.py
+++ codestar/settings.py
@@ -11,6 +11,10 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
+if os.path.isfile('env.py'):
+    import env
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -29,6 +33,9 @@ ALLOWED_HOSTS = ['8000-alvor1991-p4blog-gl7dwm0z9r0.ws.codeinstitute-ide.net',
                  '.herokuapp.com'
 ]
 
+CSRF_TRUSTED_ORIGINS = [
+    'https://8000-alvor1991-p4blog-gl7dwm0z9r0.ws.codeinstitute-ide.net'
+]
 
 # Application definition
 
@@ -76,13 +83,16 @@ WSGI_APPLICATION = 'codestar.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
-}
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': BASE_DIR / 'db.sqlite3',
+#    }
+# }
 
+DATABASES = {
+    'default': dj_database_url.parse(os.environ.get("DATABASE_URL"))
+}
 
 # Password validation
 # https://docs.djangoproject.com/en/4.2/ref/settings/#auth-password-validators
