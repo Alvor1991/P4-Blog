58cfa3c6702a51fd0b27329d9fb25aac6d105f2e
Ryan Nolan <ryannolan_@hotmail.com>
2024-06-15 09:57:36 +0000

    Apply changes to model and migrate these changes

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.utils import timezone
 
 STATUS = ((0, "Draft"), (1, "Published"))
 
@@ -9,8 +10,11 @@ class Post(models.Model):
     slug = models.SlugField(max_length=200, unique=True)
     author = models.ForeignKey(
     User, on_delete=models.CASCADE, related_name="blog_posts"
-)
-content = models.TextField()
-created_on = models.DateTimeField(auto_now_add=True)
-status = models.IntegerField(choices=STATUS, default=0)
-excerpt = models.TextField(blank=True)
+    )
+    content = models.TextField(default='')
+    created_on = models.DateTimeField(default=timezone.now)
+    status = models.IntegerField(choices=STATUS, default=0)
+    excerpt = models.TextField(blank=True)
+    updated_on = models.DateTimeField(auto_now=True)
+
+
